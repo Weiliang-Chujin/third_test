bb51ddb5ab9add146e62017ba39ed18909e8b2e8
89trillion-hehuan <hehuan@89trillion.com>
2021-08-06 11:16:47 +0800

    第三题初始化

---
+++ Assets/ThirdParty/OSA/Scripts/CustomParams/BaseParamsWithPrefabII.cs
@@ -0,0 +1,21 @@
+﻿using Com.TheFallenGames.OSA.CustomParams;
+
+[System.Serializable]
+public class BaseParamsWithPrefabII : BaseParamsWithPrefab
+{
+    /// <summary>
+    /// if true, then apply size from prefab property
+    /// </summary>
+    public bool ApplyItemSize = true;
+
+    protected override void InitItemPrefab()
+    {
+        if (ApplyItemSize)
+        {
+            AssertValidWidthHeight(ItemPrefab);
+            
+            _ItemPrefabSize = -1f; // so the prefab's size will be recalculated next time is accessed
+            DefaultItemSize = ItemPrefabSize;
+        }
+    }
+}
